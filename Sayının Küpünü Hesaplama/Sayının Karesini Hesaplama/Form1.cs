d26614fc7b6efecc950fa6d75e976707b6082f0a
Hogulcan <87578810+Hogulcan@users.noreply.github.com>
2021-11-19 19:28:43 +0300

    Add files via upload

---
+++ Sayının Küpünü Hesaplama/Sayının Karesini Hesaplama/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayının_Karesini_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            sayi = Convert.ToInt32(textBox1.Text);
+            int sayi2 = sayi * sayi * sayi;
+            MessageBox.Show(sayi2.ToString());
+        }
+    }
+}
