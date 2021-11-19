01afcf1cdc7dde6c5ba3447d797dacbdb0d256ad
Hogulcan <87578810+Hogulcan@users.noreply.github.com>
2021-11-19 19:12:45 +0300

    Add files via upload

---
+++ Giriş Paneli/Giriş Paneli/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giriş_Paneli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 form2 = new Form2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yuzde;
+            string kullanici_adi,sifre;
+            kullanici_adi = textBox1.Text;
+            sifre = textBox2.Text;
+            if(kullanici_adi == "admin" && sifre == "123456")
+            {
+                MessageBox.Show("Giriş Başarılı.Dolumun tamamlanmasını bekleyiniz.");
+                for (yuzde = 0; yuzde < 101;)
+                {
+                    yuzde = yuzde + 2;
+                    progressBar1.Value = yuzde;
+                    if (progressBar1.Value == 100)
+                    {
+                        MessageBox.Show("Dolum Tamamlandı.Yönlendiriliyorsunuz.");
+                        this.Hide();
+                        form2.Show();
+                    }
+                }
+            }
+        }
+    }
+}
