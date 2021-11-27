dfa69b2bc3211470fc266bddded5672c15a360ae
Hogulcan <87578810+Hogulcan@users.noreply.github.com>
2021-11-27 12:35:17 +0300

    Add files via upload

---
+++ Haftanın Günlerine Göre Renk Değiştirme/Haftanın Günlerine Göre Renk Değiştirme/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Haftanın_Günlerine_Göre_Renk_Değiştirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pazartesi",
+            "Salı",
+            "Çarşamba",
+            "Perşembe",
+            "Cuma",
+            "Cumartesi ",
+            "Pazar"});
+            this.comboBox1.Location = new System.Drawing.Point(316, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Haftanın Günlerine Göre Renk Değiştirme/Haftanın Günlerine Göre Renk Değiştirme/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Haftanın_Günlerine_Göre_Renk_Değiştirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Pazartesi");
+            comboBox1.Items.Add("Salı");
+            comboBox1.Items.Add("Çarşamba");
+            comboBox1.Items.Add("Perşembe");
+            comboBox1.Items.Add("Cuma");
+            label1.Text = "Bugün Günlerden : " + comboBox1.SelectedItem;
+            if(comboBox1.SelectedItem == "Pazartesi" || comboBox1.SelectedItem == "Salı" || comboBox1.SelectedItem == "Çarşamba" || comboBox1.SelectedItem == "Perşembe" || comboBox1.SelectedItem == "Cuma")
+            {
+                this.BackColor = Color.Red;
+            }
+            if(comboBox1.SelectedItem == "Cumartesi" || comboBox1.SelectedItem == "Pazar")
+            {
+                this.BackColor = Color.LimeGreen;
+            }
+            }
+        }
+    }
