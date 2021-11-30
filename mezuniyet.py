8a7810d58117e64de1fd1f2fa709119bd5076f3b
Hogulcan <87578810+Hogulcan@users.noreply.github.com>
2021-11-30 21:22:38 +0300

    Delete mezuniyet.py

--- mezuniyet.py
+++
@@ -1,8 +0,0 @@
-yas = int(input("Yaşınızı giriniz: "))
-if yas<40:
-    mezuniyet = input("Üniversite mezunumusunuz? (E/H)")
-    surucu_belgesi = input("Sürücü belgeniz var mı? (E/H)")
-if mezuniyet=="E" and surucu_belgesi=="E":
-        print("Tebrikler işe alındınız")
-else:
-        print("İşe alınmadınız")
