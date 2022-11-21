8d853fda6e3cffdf46c4f4914b59e167d0bba2c7
MARESAL <87578810+Maresal47@users.noreply.github.com>
2022-11-22 00:16:01 +0300

    Update Calcuate Circle.c

--- Calcuate Circle.c
+++ Calcuate Circle.c
@@ -4,7 +4,7 @@ int main(){
 	int r;
 	printf("You enter r: ");
 	scanf("%d",&r);
-	printf("Circle Area: %d",PI * r * r);
+	printf("Circle Area: %d",PI * (r * r));
 	printf("Circle Enviroment: %d",2 * PI * r);
 	return 0;
 }
